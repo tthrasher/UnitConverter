480034925a756d765ee5fb974004d9d6eee8020f
tthrasher <tthrasher@gmail.com>
2021-04-05 16:10:46 -0400

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Terry Thrasher on 2021-04-05.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
